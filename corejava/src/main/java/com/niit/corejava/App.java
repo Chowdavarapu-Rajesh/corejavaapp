95f73d3f0021ab321750c49d956539bb22a02ba5
Chowdavarapu-Rajesh <rajesh.ch1993@gmail.com>
2016-12-13 10:43:36 +0530

    corejava initialised

---
+++ corejava/src/main/java/com/niit/corejava/App.java
@@ -0,0 +1,13 @@
+package com.niit.corejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
